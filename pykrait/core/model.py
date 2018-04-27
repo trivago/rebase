1a033aca9e13c0f1002f4ad834c247de94324fa8
Locustv2 <locustv2@gmail.com>
2018-04-27 18:18:08 +0200

    Added context and scenarios to model

--- pykrait/core/model.py
+++ pykrait/core/model.py
@@ -1,27 +1,48 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import simplejson as json
 from pykrait.core import Object, Validator
 
 
 class Model(Object):
-    def __init__(self, **attributes):
-        super().__init__(**attributes)
+    def __init__(self, context=None, **attributes):
         self._errors = {}
+        self._context = context
+        super().__init__(**attributes)
 
     def __str__(self) -> str:
-        return json.dumps({**self.attributes, **{'_errors': self._errors}})
+        return json.dumps({
+            **self.attributes,
+            **{
+                '_errors': self._errors,
+                '_context': self._context
+            }
+        })
 
     def _properties(self) -> List[str]:
-        return [*super()._properties(), '_errors']
+        return [*super()._properties(), '_errors', '_context']
+
+    @property
+    def attributes(self) -> Dict[str, Any]:
+        context_attributes = self.scenarios().get(self._context)
+        return super().attributes if not context_attributes else {
+            k: v
+            for k, v in super().attributes.items()
+            if context_attributes and k in context_attributes
+        }
 
     def rules(self) -> Dict[str, List[Validator]]:
+        return {}
+
+    def scenarios(self) -> Dict[str, List[str]]:
         return {}
 
     def validate(self) -> bool:
         is_valid = True
 
         for attr, ruleset in self.rules().items():
+            if attr not in self.attributes:
+                continue
             for rule in ruleset:
                 value = getattr(self, attr, None)
                 if rule.required or value is not None:
@@ -36,3 +57,6 @@ class Model(Object):
 
     def get_errors(self, attribute=None) -> Dict[str, List[str]]:
         return self._errors.get(attribute) if attribute else self._errors
+
+    def get_context(self):
+        return self._context
