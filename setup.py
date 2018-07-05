da41e96851f0e1a715f7c80c6fe660b29a158049
locustv2 <locustv2@gmail.com>
2018-07-05 13:45:01 +0200

    Updated setup.py and added name to init

--- setup.py
+++ setup.py
@@ -22,9 +22,12 @@ class Pytest(TestCommand):
 setup(
     name='rebase',
     python_requires='>=3.6',
+    author="Yuvrajsingh Joodhisty",
+    author_email="locustv2@gmail.com",
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     install_requires=[
@@ -33,4 +36,8 @@ setup(
     tests_require=['pytest-cov', 'pytest', 'mock'],
     cmdclass={'test': Pytest},
     test_suite='tests',
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    )
 )
