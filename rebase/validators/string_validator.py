864b8a8bde910a965bdf405c2e463576e5b63dde
locustv2 <locustv2@gmail.com>
2018-07-09 16:43:51 +0200

    Added StringValidator and AlnumValidator

---
+++ rebase/validators/string_validator.py
@@ -0,0 +1,31 @@
+"""This file is part of the trivago/rebase library.
+
+# Copyright (c) 2018 trivago N.V.
+# License: Apache 2.0
+# Source: https://github.com/trivago/rebase
+# Version: 0.2a1
+# Python Version: 3.6
+# Author: Yuv Joodhisty <yuvrajsingh.joodhisty@trivago.com>
+"""
+
+from rebase.core import Validator
+
+
+class StringValidator(Validator):
+    def properties(self):
+        return {
+            **super().properties(),
+            'message': '`{value}` is not a valid string'
+        }
+
+    def validate(self, value):
+        if not super().validate(value):
+            return False
+
+        is_valid = True
+
+        if type(value) is not str:
+            self.errors.append(self.message.format(value=value))
+            is_valid &= False
+
+        return is_valid
