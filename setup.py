d2b707e2c98e9cb595d087c7921f01266d5b8077
locustv2 <locustv2@gmail.com>
2018-07-23 15:56:50 +0200

    Updated version to 1.0.3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ class Pytest(TestCommand):
 
 setup(
     name='rebase',
-    version='1.0.2',
+    version='1.0.3',
     python_requires='>=3.6',
     author="Yuv Joodhisty",
     author_email="locustv2@gmail.com",
